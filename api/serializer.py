440ae33ce0eb760ab68f5c4753903dfa8781c5c7
KedarBhame01 <kedarbhame1999@gmail.com>
2025-05-21 07:45:34 +0530

    API usage-GET, POST, DELETE
    in one function name 'user_detail'  use get, post, delete

--- api/serializer.py
+++ api/serializer.py
@@ -3,5 +3,5 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        modal = User
-        field = '__all__'
+        model = User
+        fields = '__all__'
